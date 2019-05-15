ea46a606258548040a8a11f3c0a39aeecce35e67
Marcos Nery <marcosnery.comp@gmail.com>
2019-05-15 02:26:56 -0300

    Adding code template

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def get_diagram():
-
-    return jsonify({"Teste":"teste"})
-
+def get_info():
+    
+    #codigos de visão computacional
+    
+    #exemplos de informações geradas
+    genApoio1 = 0;
+    genApoio2 = 2;
+    carga1 = 20;
+    posicaoApoio1=4;
+    
+    #retorna, em json, as informações adquiridas
+    return jsonify({"genApoio1": genApoio1, "genApoio2":genApoio2,"carga1":carga1,"posicaoApoio1":posicaoApoio1})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
