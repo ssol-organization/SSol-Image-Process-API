d2c58198d74ef70a177e4d857d8843a2d507cf71
Pedro Igor <pedroigor@aluno.unb.br>
2019-06-25 23:35:41 -0300

    Adicionando outros arquivos.

--- image_processing/interpret.py
+++ image_processing/interpret.py
@@ -1,55 +1,105 @@
-def interpret(all_qr_codes):
-    apoio1 = []  # cada apoio terá posição inicial, final
-    apoio2 = []
-    triangular = []  # cada triangulo: (pos_i(x, y), pos_f(x, y), peso)
-    distribuida = []
-    pontual = []  # cada pontual: (pos_i(x, y), pos_f(x, y), peso)
-    i = 0
-    begin = []
-    end = []
-    peso = 0
-    finish_triangulo = False
-    finish_distribuida = False
-    while i < len(all_qr_codes):
-        if len(all_qr_codes[i]) > 0:
-            if 'triang' in all_qr_codes[i][0][0]:
-                finish_triangulo = not finish_triangulo
-                peso = all_qr_codes[i][0][1]
-                if len(all_qr_codes[i]) == 1:
-                    begin = [i * 10, 10]
-                else:
-                    y = 0
-                    if all_qr_codes[i][0][1] == 10:
-                        y = 40
-                    elif all_qr_codes[i][0][1] == 6:
-                        y = 30
-                    else:
-                        y = 20
-                    end = [(i + 1) * 10, y]
-                if not finish_triangulo:
-                    triangular.append([begin, end, peso])  # triangular = (pos_i(x, y), pos_f(x, y), peso)
+def interpret_viga(viga):
+    return [viga[0][0] + viga[0][2], viga[1][0]]
 
-            elif 'pontual' in all_qr_codes[i][0][0]:
-                altura = all_qr_codes[i][0][1] * 10
-                peso = all_qr_codes[i][0][1]
-                pontual.append(
-                    [i * 10, (i + 1) * 10, altura, peso])  # pontual = (pos_i(x, y), pos_f(x, y), peso)
-                # os y são iguais por ser no máximo uma coluna
 
-            elif 'apoio 2' in all_qr_codes[i][0][0]:  # apoio = (xi, xf)
-                pos = [i * 10, (i + 1) * 10]
-                apoio2.append(pos)
+def interpret_distribuida(distribuida: list):
+    forcas_distribuidas = []
+    inicio = 0
+    fim = 0
+    if len(distribuida):
+        if distribuida[0][2][0] > distribuida[1][2][0]:
+            fim = distribuida[0][2][0] + distribuida[0][2][2]
+            inicio = distribuida[1][2][0]
+        else:
+            fim = distribuida[1][2][0] + distribuida[1][2][2]
+            inicio = distribuida[0][2][0]
+        weight = str(distribuida[0][0]).replace("\'", "").split('w: ')
+        forcas_distribuidas.append([int(weight[1]), inicio, fim])
+    return forcas_distribuidas
 
-            elif 'apoio 1' in all_qr_codes[i][0][0]:  # apoio = (xi, xf)
-                pos = [i * 10, (i + 1) * 10]
-                apoio1.append(pos)
-            elif 'distr' in all_qr_codes[i][0][0]:
-                peso = all_qr_codes[i][0][1]
-                if not finish_distribuida:
-                    begin = i * 10
-                    finish_distribuida = not finish_distribuida
-                else:
-                    end = (i + 1) * 10
-                    distribuida.append([begin, end, peso])
-        i += 1
-    return [apoio1, apoio2, pontual, distribuida, triangular]
+
+def interpret_pontual(pontual):
+    forcas_pontuais = []
+    for p in pontual:
+        weight = str(pontual[0][0]).replace("\'", "").split('w: ')
+        forcas_pontuais.append([int(weight[1]), p[2][0], p[2][0] + p[2][2]])
+
+    return forcas_pontuais
+
+
+def interpret_triangular(triangular):
+    triangular_10 = []
+    triangular_6 = []
+    triangular_3 = []
+    forcas_triangulares = []
+    for t in triangular:
+        if 'w: 10' in str(t[0]):
+            triangular_10.append(t[2])
+        elif 'w: 6' in str(t[0]):
+            triangular_6.append(t[2])
+        elif 'w: 3' in str(t[0]):
+            triangular_3.append(t[2])
+
+    if len(triangular_10):
+        mais_distante = 0
+        mais_proximo = 0
+        outro = 0
+        width = 0
+        mais_distante = max(triangular_10[0][0], triangular_10[1][0], triangular_10[2][0])
+        mais_proximo = min(triangular_10[0][0], triangular_10[1][0], triangular_10[2][0])
+        for t in triangular_10:
+            if t[0] != mais_distante and t[0] != mais_proximo:
+                outro = t[0]
+            if t[0] == mais_distante:
+                width = t[2]
+        if abs(outro - mais_proximo) >= 50:
+            forcas_triangulares.append([10, mais_proximo, mais_distante + width])
+        else:
+            forcas_triangulares.append([10, mais_distante + width, mais_proximo])
+
+    if len(triangular_6):
+        mais_distante = 0
+        mais_proximo = 0
+        outro = 0
+        width = 0
+        mais_distante = max(triangular_6[0][0], triangular_6[1][0], triangular_6[2][0])
+        mais_proximo = min(triangular_6[0][0], triangular_6[1][0], triangular_6[2][0])
+        for t in triangular_6:
+            if t[0] != mais_distante and t[0] != mais_proximo:
+                outro = t[0]
+            if t[0] == mais_distante:
+                width = t[2]
+        if abs(outro - mais_proximo) >= 10:
+            forcas_triangulares.append([6, mais_proximo, mais_distante + width])
+        else:
+            forcas_triangulares.append([6, mais_distante + width, mais_proximo])
+
+    if len(triangular_3):
+        mais_distante = 0
+        mais_proximo = 0
+        outro = 0
+        width = 0
+        mais_distante = max(triangular_3[0][0], triangular_3[1][0], triangular_3[2][0])
+        mais_proximo = min(triangular_3[0][0], triangular_3[1][0], triangular_3[2][0])
+        for t in triangular_3:
+            if t[0] != mais_distante and t[0] != mais_proximo:
+                outro = t[0]
+            if t[0] == mais_distante:
+                width = t[2]
+        if abs(outro - mais_proximo) >= 10:
+            forcas_triangulares.append([3, mais_proximo, mais_distante + width])
+        else:
+            forcas_triangulares.append([3, mais_distante + width, mais_proximo])
+
+    return forcas_triangulares
+
+
+def interpret_apoios(apoios):
+    apoio_1 = []
+    apoio_2 = []
+    for a in apoios:
+        if 'apoio 1' in str(a[0]):
+            apoio_1.append([a[2][0], a[2][0] + a[2][2]])
+        elif 'apoio 2' in str(a[0]):
+            apoio_2.append([a[2][0], a[2][0] + a[2][2]])
+    return apoio_1, apoio_2
