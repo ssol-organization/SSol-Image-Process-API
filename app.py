90d6266034c3539b636c6a2c328a7f5d9d897f4c
Pedro Igor <pedroigor@aluno.unb.br>
2019-06-30 22:16:09 -0300

    Atualizando json das cargas.

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def receive_image():
 
 def get_info():
     try:
-        image = cv2.imread('img/pontual_4_new.png')[:, :, ::-1]
+        image = cv2.imread('img/triangular_10_new.png')[:, :, ::-1]
         image = pre_processing(image)
         data = decode(image)
         viga = find_viga(data)
@@ -49,9 +49,6 @@ def get_info():
         triangulares_data = interpret_triangular(triangular)
         pontuais_data = interpret_pontual(pontual)
         distribuidas_data = interpret_distribuida(distribuida)
-        dict_apoio2 = {}
-        posicao_i = {}
-        posicao_f = {}
         tipo1 = {}
         tipo2 = {}
         tipo1["tipo"] = 0
@@ -60,17 +57,23 @@ def get_info():
         for a in apoio2:
             apoios.append([tipo2, {"posicao_i": a[3][0][0]}, {"posicao_f": a[3][3][0]}])
             apoios = copy.copy(apoios)
-            print(apoios[-1])
-            posicao_i.clear()
-            posicao_f.clear()
         for a in apoio1:
             apoios.append([tipo1, {"posicao_i": a[3][0][0]}, {"posicao_f": a[3][3][0]}])
             apoios = copy.copy(apoios)
-            print(apoios[-1])
-            posicao_i.clear()
-            posicao_f.clear()
+        cargas_pontuais = []
+        for c in pontuais_data:
+            cargas_pontuais.append([{"posicao_i":c[1], "posicao_f":c[2], "modulo": c[0]}])
 
-        return jsonify(posicao_viga = viga_data, apoios = apoios, cargas_pontuais = pontuais_data, cargas_distribuidas = distribuidas_data, cargas_triangulares = triangulares_data)
+        cargas_distribuidas = []
+        for c in distribuidas_data:
+            cargas_distribuidas.append([{"posicao_i":c[1], "posicao_f":c[2], "modulo": c[0]}])
+
+        cargas_triangulares = []
+        for c in triangulares_data:
+            print(c)
+            cargas_triangulares.append([{"posicao_i":c[1], "posicao_f":c[2], "modulo": c[0]}])
+
+        return jsonify(PosicaoViga = [{"inicial":viga_data[0]}, {"final": viga_data[1]}], apoios = apoios, cargasP = cargas_pontuais, cargasD = cargas_distribuidas, cargasT = cargas_triangulares)
     except:
         return "Erro ao extrair informações da viga."
 
